81e4ff5f1f15a21ef168a596eaefcb308ec1af41
Travassa <a32977@alunos.ipca.pt>
2024-11-21 23:41:04 +0000

    create new form and add mongo driver

---
+++ eletronicMall/DataBase/DataBaseConnection.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eletronicMall.DataBase
+{
+    public class DataBaseConnection
+    {
+        private IMongoDatabase _database;
+        private void InitializeMongoDB()
+        {
+            try
+            {
+                // Connection string (modify as needed for your MongoDB setup)
+                //"mongodb+srv://<db_username>:<db_password>@projectipca.qsiry.mongodb.net/?retryWrites=true&w=majority&appName=projectIpca"; // Change to your connection string
+                string connectionString = "mongodb+srv://sandro:afa321321987654@projectipca.qsiry.mongodb.net/?retryWrites=true&w=majority&appName=projectIpca"; // Change to your connection string
+                var client = new MongoClient(connectionString);
+
+                // Get the database (replace 'testdb' with your database name)
+                _database = client.GetDatabase("testdb");
+
+                MessageBox.Show("Connected to MongoDB successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"MongoDB connection failed: {ex.Message}");
+            }
+        }
+        private void FetchData()
+        {
+            try
+            {
+                // Replace 'collectionName' with your actual collection name
+                var collection = _database.GetCollection<BsonDocument>("collectionName");
+
+                // Fetch all documents in the collection
+                var documents = collection.Find(new BsonDocument()).ToList();
+
+                // Display data (for demonstration)
+                foreach (var doc in documents)
+                {
+                    MessageBox.Show(doc.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error fetching data: {ex.Message}");
+            }
+        }
+    }
+
+}
---
+++ eletronicMall/forms/ProductsForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace eletronicMall.forms
+{
+    partial class ProductsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ColumnHeader name;
+            ColumnHeader qtd;
+            listView1 = new ListView();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            btnAdd = new Button();
+            btnClean = new Button();
+            id = new ColumnHeader();
+            name = new ColumnHeader();
+            qtd = new ColumnHeader();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { id, name, qtd });
+            listView1.Location = new Point(12, 152);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(776, 286);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnClean);
+            groupBox1.Controls.Add(btnAdd);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(12, 16);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(774, 130);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(62, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(166, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(320, 29);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(170, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(576, 29);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(168, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(62, 91);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(166, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(323, 92);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(167, 27);
+            textBox5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(264, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(520, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 6;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 95);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 7;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(264, 98);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 8;
+            label5.Text = "label5";
+            label5.Click += label5_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(545, 91);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 9;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnClean
+            // 
+            btnClean.Location = new Point(650, 91);
+            btnClean.Name = "btnClean";
+            btnClean.Size = new Size(94, 29);
+            btnClean.TabIndex = 10;
+            btnClean.Text = "Clean";
+            btnClean.UseVisualStyleBackColor = true;
+            // 
+            // id
+            // 
+            id.Text = "id";
+            // 
+            // name
+            // 
+            name.Text = "name";
+            name.Width = 120;
+            // 
+            // qtd
+            // 
+            qtd.Text = "Quantite";
+            qtd.Width = 90;
+            // 
+            // ProductsForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(listView1);
+            Name = "ProductsForm";
+            Text = "ProductsForm";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private GroupBox groupBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox5;
+        private Button btnClean;
+        private Button btnAdd;
+        private ColumnHeader id;
+    }
+}
